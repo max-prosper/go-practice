e3fdc81a2209d80b20af597319f1f88b1815292f
Max Prosper <maxim.yaaman@gmail.com>
2017-12-16 00:35:40 +0200

    Iterating over maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	//var colors map[string]string
+	//colors := make(map[string]string)
+
+	//colors["white"] = "#ffffff"
+
+	//delete(colors, "white")
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
