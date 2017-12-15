c6d398208a112b6d34431f0362bfae42f8561966
Max Prosper <maxim.yaaman@gmail.com>
2017-12-15 23:34:02 +0200

    Structs with Receiver functions

--- structs/main.go
+++ structs/main.go
@@ -10,18 +10,26 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
 
-	fmt.Printf("%+v", jim)
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
