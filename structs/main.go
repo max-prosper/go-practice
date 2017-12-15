3606f1b163d209407bcb9e6c2fefa53d84c9702b
Max Prosper <maxim.yaaman@gmail.com>
2017-12-15 23:18:09 +0200

    Intro to Structs

---
+++ structs/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+// DECLARATION VARIANT #1
+// func main() {
+// 	alex := person{firstName: "Alex", lastName: "Anderson"}
+// 	fmt.Println(alex)
+// }
+
+// DECLARATION VARIANT #2
+func main() {
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	fmt.Printf("%+v", alex)
+}
